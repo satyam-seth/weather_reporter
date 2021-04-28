b8855b123fa761fc1096bca621a783cc9d3e4911
Satyam Seth <satyam1998.1998@gmail.com>
2021-04-29 04:23:17 +0530

    fix post request response

--- weather/core/views.py
+++ weather/core/views.py
@@ -6,23 +6,23 @@ from .forms import SearchForm
 
 # Create your views here.
 
-def get_report(city):
+def get_report(loc):
     USER_AGENT="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
     LANGUAGE="en-US,en;q=0.5"
     session=requests.Session()
     session.headers['User-Agent']=USER_AGENT
     session.headers['Accept-Language']=LANGUAGE
     session.headers['Content-Language']=LANGUAGE
-    city=city.replace(' ','+')
+    loc=loc.replace(' ','+')
     result=dict()
     try:
-        html_content=session.get(f'https://www.google.com/search?q=weather+in+{city}').text
+        html_content=session.get(f'https://www.google.com/search?q=weather+in+{loc}').text
         soup=BeautifulSoup(html_content,'html.parser')
         result['region']=soup.find("span",attrs={"class":"BNeawe tAd8D AP7Wnd"}).text
         result['temp_now']=soup.find("div",attrs={"class":"BNeawe iBp4i AP7Wnd"}).text
         result['day_hour'],result['weather_now']=soup.find("div",attrs={"class":"BNeawe tAd8D AP7Wnd"}).text.split('\n')
     except:
-        result['error']='invalid location'
+        pass
     return result
 
 # print(get_report(input()))
@@ -30,10 +30,17 @@ def get_report(city):
 class HomeView(View):
     def get(self,request):
         fm=SearchForm()
-        return render(request,'core/home.html',{'form':fm})
+        context={'form':fm}
+        return render(request,'core/home.html',context)
 
     def post(self,request):
+        context={}
         fm=SearchForm(request.POST)
         if fm.is_valid():
-            print(fm.cleaned_data['loc'])
-        return render(request,'core/home.html',{'form':fm})
+            result=get_report(fm.cleaned_data['loc'])
+        if result:
+            context['result']=result
+        else:
+            context['error']='invalid location'
+        context['form']=fm
+        return render(request,'core/home.html',context)
